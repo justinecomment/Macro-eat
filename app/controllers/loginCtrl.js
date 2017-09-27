87a69eda1c9b8c203db8a2fbc24da8d283d131c4
Justine <justinecomment@gmail.com>
2017-09-28 00:20:14 +0200

    + Check if username exist.

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -1,29 +1,32 @@
-myApp.controller('loginCtrl', function($scope, $location, loginService) {
+myApp.controller('loginCtrl', function($scope, $location, loginService, LxNotificationService) {
    
   $scope.createUser = function(){
       if($scope.userForm.$valid === true){
         var userToPost = {
-            'username' : document.getElementById('username'),
-            'password' : document.getElementById('password')
+            'username' : document.getElementById('username').value,
+            'password' : document.getElementById('password').value
         }
-        
         loginService.postUser(userToPost).then(function(result){
-            $scope.createUser = result.data;
+            console.log(result);
+            LxNotificationService.notify('Compte de ' + userToPost.username + ' crée', undefined, undefined, undefined, undefined, undefined, 2 * 2000);
+
+        }).catch(function(result){
+            console.log(result.data);
+            LxNotificationService.notify('Username existant', undefined, undefined, undefined, undefined, undefined, 2 * 2000);
+
         })
+
       }
   };
 
   $scope.login = function(){
       if($scope.userForm.$valid === true){
         var dataUser = {
-            'username' : $scope.username,
-            'password' : $scope.password
+            'username' : document.getElementById('username').value,
+            'password' :document.getElementById('password').value
         }
-
-        loginService.getUser(dataUser).then(function(result){
-            $scope.login = result.data;
-            
-        })
+        loginService.getUser(dataUser);
+        LxNotificationService.notify('Bonjour ' + dataUser.username , undefined, undefined, undefined, undefined, undefined, 2 * 2000);
       }
   };
 
--- app/services/loginService.js
+++ app/services/loginService.js
@@ -1,30 +1,29 @@
-myApp.service('loginService', function($http){
+myApp.service('loginService', function($http, $location){
     const baseUrl = 'http://localhost/macroeat';
    
 
     this.postUser = function(userToPost){
-        return $http({ 
-            method: 'POST', 
-            url: baseUrl + '/login.php?register=', 
-            dataType: 'json', 
-            data: userToPost,
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        });
+        return $http.post(baseUrl + '/login.php?registerUsername=' + userToPost.username + "&password=" + userToPost.password, userToPost )
+            .success(function(data, status, headers, config) {
+                return result.data;
+            })
+            .error(function(data, status, headers, config) {
+                return data;
+            });
+
     };
 
+
+ 
+
     this.getUser = function(dataUser){
-         return $http.get(baseUrl + '/login.php?username=' + dataUser.username + "&password=" + dataUser.password ).success(function(result){
+         return $http.get(baseUrl + '/login.php?loginUsername=' + dataUser.username + "&password=" + dataUser.password ).success(function(result){
             document.getElementById("success").innerHTML = "Bienvenue " + result;
+            $location.path('/accueil');
              return result;
         }).error(function(data){
            document.getElementById("error").innerHTML = "Mauvais Login/Mot de passe. Veuillez recommencer.";
          });
     };
-
-
-
-
 
 });
